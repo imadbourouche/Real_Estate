7e56a932d555614a43ddd59c88b5508d9f8490f6
imadbourouche <ji_bourouche@esi.dz>
2023-07-14 18:27:01 +0100

    Init Module

---
+++ __manifest__.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Estate",
+    'summary': """Real estate management""",
+    'description': """
+        Real estate management module
+    """,
+    'license': 'LGPL-3',
+    'author': "Imad Bourouche",
+    'email': "ji_bourouche@esi.dz",
+    'category': 'Learn',
+    'version': '0.1',
+    'application': True,
+    #'depends': [],
+    'data': [],
+    'demo': [],
+}
