936ff250d75792f5b3aeed2e12ecb438aa7ede04
imadbourouche <ji_bourouche@esi.dz>
2023-08-19 16:04:55 +0100

    Update init and manifest

--- __manifest__.py
+++ __manifest__.py
@@ -8,10 +8,22 @@
     'license': 'LGPL-3',
     'author': "Imad Bourouche",
     'email': "ji_bourouche@esi.dz",
-    'category': 'Learn',
+    'category': 'Administration',
     'version': '0.1',
     'application': True,
     #'depends': [],
-    'data': [],
-    'demo': [],
+    'data': [
+        #'data/estate.property.type.csv',
+        #'data/estate.property.csv',
+        'security/ir.model.access.csv',
+        'views/estate_property_type_views.xml',
+        'views/estate_property_tag_views.xml',
+        'views/estate_property_offer_views.xml',
+        'views/estate_property_views.xml',
+
+        'views/estate_menus.xml'
+    ],
+    #'demo': [
+    #'demo.xml',
+    #],
 }
